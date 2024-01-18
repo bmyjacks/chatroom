79a0118711e0794ce8d59a38fb69ab60c918afde
bmyjacks <dev@bmyjacks.io>
2024-01-18 21:22:09 +0800

    build: add dependency lanterna for console ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("java")
 }
 
@@ -7,9 +8,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
+    implementation("com.googlecode.lanterna:lanterna:3.1.1")
+    implementation("info.picocli:picocli:4.7.5")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -20,4 +24,21 @@ tasks.test {
 
 tasks.withType<Javadoc> {
     setDestinationDir(file("doc"))
+}
+
+tasks.getByName<JavaCompile>("compileJava") {
+    options.compilerArgs.plusAssign(listOf("-Aproject=${project.group}/${project.name}"))
+}
+
+tasks.getByName<Jar>("jar") {
+    manifest {
+        attributes["Main-Class"] = "io.bmyjacks.app.chatroom.Main"
+    }
+}
+
+tasks.getByName<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    manifest {
+        attributes["Main-Class"] = "io.bmyjacks.app.chatroom.Main"
+    }
+    minimize()
 }
