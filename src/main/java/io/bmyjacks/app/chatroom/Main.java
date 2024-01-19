a4b43078ba8b72ced4bf1572ad67e6c836937399
bmyjacks <dev@bmyjacks.io>
2024-01-19 10:46:13 +0800

    feat: adapt main.java to function as the portal of chatroom

--- src/main/java/io/bmyjacks/app/chatroom/Main.java
+++ src/main/java/io/bmyjacks/app/chatroom/Main.java
@@ -1,16 +1,50 @@
 package io.bmyjacks.app.chatroom;
 
-/**
- * The Main class serves as the entry point for the application.
- */
+import io.bmyjacks.app.chatroom.client.Client;
+import io.bmyjacks.app.chatroom.server.Server;
+import picocli.CommandLine;
+
+import java.io.IOException;
+
+@CommandLine.Command(description = "A simple chatroom application", mixinStandardHelpOptions = true, version = "1.0")
 public class Main {
-    /**
-     * The main method is the entry point for the Java application.
-     * It prints a welcome message to the console.
-     *
-     * @param args The command line arguments. Not used in this application.
-     */
-    public static void main(String... args) {
-        System.out.print("Hello and welcome!");
+    @CommandLine.Option(names = {"-s", "--server"}, description = "Run as server")
+    static boolean isServer;
+    @CommandLine.Option(names = "-p", defaultValue = "54736", description = "The port to use. Default:${DEFAULT-VALUE}")
+    static int port;
+    @CommandLine.Option(names = {"-h", "--help"}, usageHelp = true, description = "Display this help")
+    boolean help;
+    @CommandLine.Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information")
+    boolean versionRequested;
+
+    public static void main(String... args) throws IOException {
+        CommandLine commandLine = new CommandLine(new Main());
+        commandLine.parseArgs(args);
+
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+            return;
+        }
+
+        if (commandLine.isVersionHelpRequested()) {
+            commandLine.printVersionHelp(System.out);
+            return;
+        }
+
+        switch (isServer ? "server" : "client") {
+            case "server":
+                System.out.println("Running as server on port " + port + "...");
+                Server server = new Server(port);
+                server.run();
+                break;
+            case "client":
+                System.out.println("Running as client on port " + port + "...");
+                Client client = new Client(port);
+                client.run();
+                break;
+            default:
+                System.out.println("Invalid option");
+                break;
+        }
     }
 }
