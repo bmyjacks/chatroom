ac2e94926c53470dc9b3c8fe186642d71001ba22
bmyjacks <dev@bmyjacks.io>
2024-01-26 11:22:29 +0800

    refactor: move clientHandler to its source file

---
+++ src/main/java/io/bmyjacks/app/chatroom/server/ClientHandler.java
@@ -0,0 +1,125 @@
+package io.bmyjacks.app.chatroom.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.StringTokenizer;
+
+class ClientHandler implements Runnable {
+    private final Socket clientSocket;
+    private final DataInputStream streamFromClient;
+    private final DataOutputStream streamToClient;
+    private String username;
+    private boolean isOnline;
+
+    protected ClientHandler(Socket socket, DataInputStream streamFromClient, DataOutputStream streamToClient) {
+        this.username = "#" + socket.getPort();
+        this.clientSocket = socket;
+        this.streamFromClient = streamFromClient;
+        this.streamToClient = streamToClient;
+        this.isOnline = true;
+    }
+
+    private void sendToAll(String message) throws IOException {
+        for (var client : Server.activeClient) {
+            client.streamToClient.writeUTF(message);
+        }
+    }
+
+    private void unsentMessage(String username) throws IOException {
+        for (int i = Server.history.size() - 1; i >= 0; i--) {
+            StringTokenizer stringTokenizer = new StringTokenizer(Server.history.get(i));
+            stringTokenizer.nextToken();
+            String messageUsername = stringTokenizer.nextToken();
+            if (messageUsername.equals(username + ":")) {
+                Server.history.set(i, username + " unsent");
+                break;
+            }
+        }
+        sendToAll("#CLR");
+        for (var historyMessage : Server.history) {
+            sendToAll(historyMessage);
+        }
+    }
+
+    private void getUsername() {
+        String userInput = null;
+        try {
+            userInput = streamFromClient.readUTF();
+        } catch (IOException e) {
+            System.out.println("Error: IOException");
+        }
+
+        if (userInput.equals("/exit")) {
+            System.exit(0);
+        }
+        username = userInput;
+    }
+
+    private void sendHistory() {
+        try {
+            for (var historyMessage : Server.history) {
+                streamToClient.writeUTF(historyMessage);
+            }
+            streamToClient.writeUTF("-------- Above is history --------");
+        } catch (IOException e) {
+            System.out.println("Error: IOException");
+        }
+    }
+
+    private void close() {
+        try {
+            streamFromClient.close();
+            streamToClient.close();
+            clientSocket.close();
+            Server.activeClient.remove(this);
+        } catch (IOException e) {
+            System.out.println("Error: IOException");
+        }
+    }
+
+    private String getFormattedCurrentTime() {
+        LocalTime time = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return "[" + time.format(formatter) + "] ";
+    }
+
+    private void processMessage(String input) throws IOException {
+        if (input.equals("/exit")) {
+            isOnline = false;
+            return;
+        }
+        if (input.equals("/undo")) {
+            unsentMessage(username);
+            return;
+        }
+
+        String message = getFormattedCurrentTime() + username + ": " + input;
+        sendToAll(message);
+        Server.history.add(message);
+    }
+
+    @Override
+    public void run() {
+        getUsername();
+
+        if (!Server.history.isEmpty()) {
+            sendHistory();
+        }
+
+
+        while (isOnline) {
+            try {
+                String userInput = streamFromClient.readUTF();
+                processMessage(userInput);
+            } catch (IOException e) {
+                System.out.println("Error: IOException");
+            }
+        }
+
+        close();
+    }
+}
--- src/main/java/io/bmyjacks/app/chatroom/server/Server.java
+++ src/main/java/io/bmyjacks/app/chatroom/server/Server.java
@@ -5,9 +5,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.StringTokenizer;
 import java.util.Vector;
 
 public class Server {
@@ -19,120 +16,23 @@ public class Server {
         this.port = port;
     }
 
-    public void run() throws IOException {
-        ServerSocket serverSocket = new ServerSocket(port);
-        Socket clientSocket;
+    public void run() throws IOException, SecurityException, IllegalArgumentException {
+        ServerSocket serverSocket;
+        serverSocket = new ServerSocket(port);
 
         while (true) {
-            clientSocket = serverSocket.accept();
-//            System.out.println("New client request received: " + socket);
+            Socket clientSocket = serverSocket.accept();
 
-            DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
-            DataOutputStream dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+            DataInputStream streamFromClient = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream streamToClient = new DataOutputStream(clientSocket.getOutputStream());
 
-//            System.out.println("Creating a new handler for this client...");
-            ClientHandler clientHandler = new ClientHandler(clientSocket, dataInputStream, dataOutputStream);
+            ClientHandler clientHandler = new ClientHandler(clientSocket, streamFromClient, streamToClient);
+
+            activeClient.add(clientHandler);
 
             Thread handlerThread = new Thread(clientHandler);
-//            System.out.println("Adding this client to active client list");
-            activeClient.add(clientHandler);
             handlerThread.start();
         }
     }
 }
 
-class ClientHandler implements Runnable {
-    private final Socket clientSocket;
-    private final DataInputStream dataInputStream;
-    private final DataOutputStream dataOutputStream;
-    private String username;
-
-    public ClientHandler(Socket socket, DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
-        this.username = "#" + socket.getPort();
-        this.clientSocket = socket;
-        this.dataInputStream = dataInputStream;
-        this.dataOutputStream = dataOutputStream;
-    }
-
-    public void sendToAll(String message) throws IOException {
-        for (var client : Server.activeClient) {
-            client.dataOutputStream.writeUTF(message);
-        }
-    }
-
-    public void unsentMessage(String username) throws IOException {
-        for (int i = Server.history.size() - 1; i >= 0; i--) {
-            StringTokenizer stringTokenizer = new StringTokenizer(Server.history.get(i));
-            stringTokenizer.nextToken();
-            String messageUsername = stringTokenizer.nextToken();
-            if (messageUsername.equals(username + ":")) {
-                Server.history.set(i, username + " unsent");
-                break;
-            }
-        }
-        sendToAll("#CLR");
-        for (var historyMessage : Server.history) {
-            sendToAll(historyMessage);
-        }
-    }
-
-    @Override
-    public void run() {
-        String userInput = null;
-
-        try {
-            username = dataInputStream.readUTF();
-        } catch (IOException e) {
-            System.out.println("Error IOException");
-        }
-
-        if (!Server.history.isEmpty()) {
-            try {
-                for (var historyMessage : Server.history) {
-                    dataOutputStream.writeUTF(historyMessage);
-                }
-                dataOutputStream.writeUTF("-------- Above is history --------");
-            } catch (IOException e) {
-                System.out.println("Error IOException");
-            }
-        }
-
-
-        while (true) {
-            try {
-                userInput = dataInputStream.readUTF();
-                if (userInput.equals("/exit")) {
-                    break;
-                }
-                if (userInput.equals("/undo")) {
-                    unsentMessage(username);
-                    continue;
-                }
-                LocalTime time = LocalTime.now();
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-                String message = "[" + time.format(formatter) + "] " + username + ": " + userInput;
-                System.out.println(message);
-                for (var client : Server.activeClient) {
-                    if (client == this) {
-                        client.dataOutputStream.writeUTF(message);
-                    } else {
-                        client.dataOutputStream.writeUTF(message);
-                    }
-                }
-                Server.history.add(message);
-            } catch (IOException e) {
-                System.out.println("Error: IOException");
-                break;
-            }
-        }
-
-        try {
-            dataInputStream.close();
-            dataOutputStream.close();
-            clientSocket.close();
-            Server.activeClient.remove(this);
-        } catch (IOException e) {
-            System.out.println("Error: IOException");
-        }
-    }
-}
