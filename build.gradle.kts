145db141753ea09e3fbebeda02eda00e1841edf3
bmyjacks <dev@bmyjacks.io>
2024-02-16 17:19:53 +0800

    build: add dependency for guava

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 dependencies {
     implementation("com.googlecode.lanterna:lanterna:3.1.1")
     implementation("info.picocli:picocli:4.7.5")
+    implementation("com.google.guava:guava:33.0.0-jre")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-core:5.10.0")
