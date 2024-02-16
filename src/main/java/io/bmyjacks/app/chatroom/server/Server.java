2c9419452ca910c1f33e0aa03e7abb401c2e6a12
bmyjacks <dev@bmyjacks.io>
2024-02-16 17:21:15 +0800

    refactor: add getter and setter for Server.java

--- src/main/java/io/bmyjacks/app/chatroom/server/Server.java
+++ src/main/java/io/bmyjacks/app/chatroom/server/Server.java
@@ -11,39 +11,44 @@ import java.util.Vector;
  * This class represents the server in the chatroom application.
  * It maintains a list of active clients and a history of messages.
  * It listens for incoming client connections and starts a new ClientHandler for each one.
+ *
+ * @param port The port the server is running on
  */
-public class Server {
-    // A history of messages
-    static Vector<Message> history = new Vector<>();
+public record Server(int port) {
     // A list of active clients
-    static Vector<ClientHandler> activeClient = new Vector<>();
-    // The port the server is running on
-    private final int port;
+    private static final Vector<ClientHandler> activeClient = new Vector<>();
+    // A history of messages
+    private static final Vector<Message> history = new Vector<>();
+    private static ServerSocket serverSocket;
 
-    /**
-     * Constructs a new Server with the specified port.
-     *
-     * @param port the port the server will run on
-     */
-    public Server(int port) {
-        this.port = port;
+    public static ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
+    public static void setServerSocket(ServerSocket serverSocket) {
+        Server.serverSocket = serverSocket;
+    }
+
+    public static Vector<ClientHandler> getActiveClient() {
+        return activeClient;
+    }
+
+    public static Vector<Message> getHistory() {
+        return history;
     }
 
     /**
      * Starts the server.
      * It listens for incoming client connections and starts a new ClientHandler for each one.
      *
-     * @throws IOException              if there is an error accepting a client connection
-     * @throws SecurityException        if a security manager exists and its checkListen method doesn't allow the operation
-     * @throws IllegalArgumentException if the port parameter is outside the specified range of valid port values
+     * @throws IOException if there is an error accepting a client connection
      */
-    public void run() throws IOException, SecurityException, IllegalArgumentException {
-        ServerSocket serverSocket;
-        serverSocket = new ServerSocket(port);
+    public void run() throws IOException {
+        setServerSocket(new ServerSocket(port));
 
         while (true) {
             // Accept a client connection
-            Socket clientSocket = serverSocket.accept();
+            Socket clientSocket = getServerSocket().accept();
 
             // Create input and output streams for the client
             DataInputStream streamFromClient = new DataInputStream(clientSocket.getInputStream());
@@ -53,7 +58,7 @@ public class Server {
             ClientHandler clientHandler = new ClientHandler(clientSocket, streamFromClient, streamToClient);
 
             // Add the client to the list of active clients
-            activeClient.add(clientHandler);
+            getActiveClient().add(clientHandler);
 
             // Start the ClientHandler in a new thread
             Thread handlerThread = new Thread(clientHandler);
